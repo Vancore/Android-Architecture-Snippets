07fb26023b0e77cc6e8225422ba9759ecf57b775
Marko Nonninger <marko.nonninger@scout24.ch>
2021-01-07 16:06:29 +0100

    Initial commit - start of the project

---
+++ Playground-App/app/src/main/java/vancore/playground/ContentActivity.kt
@@ -0,0 +1,17 @@
+package vancore.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
